00838ca67fc53502533fbc1a9cbc7f2a0c55cb95
Alexandr Bochev <oleksandr.b@fastbackrefunds.com>
2025-01-13 13:24:19 +0200

    extend screen

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 export default function Home() {
   return (
     <main className="w-full md:h-svh flex items-center container mx-auto py-5 md:py-7.5 text-primary">
-      <div className="relative w-full flex flex-col items-end justify-center md:flex-row">
+      <div className="relative w-full flex flex-col items-end justify-start md:flex-row">
         <Hero />
         <Suspense fallback={<div>Loading...</div>}>
           <CreateAccount />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,17 @@ export default {
   theme: {
     screens: {
       sm: "768px",
-      md: "1024px",
-      lg: "1536px",
-      // md: "1200",
-      // lg: "1800",
+      md: "1200",
+      lg: "1800",
     },
     extend: {
+      screens: {
+        sm: "768px",
+        // md: "1024px",
+        // lg: "1536px",
+        md: "1200",
+        lg: "1800",
+      },
       colors: {
         background: "#201818",
         accent: "#E80004",
